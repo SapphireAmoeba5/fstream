50c722eeb99a4507f7c2750971c12bf6c55f92ca
SapphireAmoeba5 <clevermail19@gmail.com>
2021-10-25 23:20:52 -0300

    Added more functionality to the class

--- include/fstream.h
+++ include/fstream.h
@@ -28,18 +28,24 @@ namespace fileio
         ~File();
 
     public: // Public methods
-        bool Write(const char* data);
-        bool Write(unsigned char* data, size_t count);
+        bool Write(const char* data, int count = -1);
+        bool Write(unsigned char* data, size_t size);
+        
+        bool Read(char* buffer, size_t count);
+        bool Read(unsigned char* buffer, size_t count);
+        
+        const size_t FileSize() const;
     public: // Getters
         inline const bool IsOpen() const {return m_Open;}
 
     private: // Private non-static variables
         FILE* m_FileHandle;
-
         bool m_Open;
         bool m_WriteMode;
+        bool m_ReadMode;
     private: // Private methods
         void Impl_OpenFile(const char* path, io::modifiers modifier);
+        inline const bool Impl_ValidHandle() {return m_FileHandle != nullptr;}
     };
 }
 
@@ -59,13 +65,13 @@ static void fopen_imp(const char* path, const char* modes, FILE*& file)
 namespace fileio
 {
     File::File(const char* path, int modifier)
-        :m_FileHandle(nullptr), m_Open(false), m_WriteMode(false)
+        :m_FileHandle(nullptr), m_Open(false), m_WriteMode(false), m_ReadMode(false)
     {
         Impl_OpenFile(path, static_cast<io::modifiers>(modifier));
 
-        if(m_FileHandle != nullptr) // File is open
+        if(Impl_ValidHandle()) // File is open
             m_Open = true;
-        else                        // File isn't open
+        else                   // File isn't open
             m_Open = false;
     }
     
@@ -83,11 +89,12 @@ namespace fileio
 
     }
 
-    bool File::Write(const char* data)
+    bool File::Write(const char* data, int count)
     {
-        size_t count = strlen(data);
+        if(count == -1)
+            count = strlen(data);
 
-        if(m_WriteMode)
+        if(m_Open && m_WriteMode)
         {
             fwrite(data, 1, count, m_FileHandle);
             return true;
@@ -96,15 +103,46 @@ namespace fileio
         return false;
     }
 
-    bool File::Write(unsigned char* data, size_t count)
+    bool File::Write(unsigned char* data, size_t size)
     {
-        if(m_WriteMode)
+        if(m_Open && m_WriteMode)
         {
-            fwrite(data, 1, count, m_FileHandle);
+            fwrite(data, 1, size, m_FileHandle);
             return true;
         }
 
         return false;
+    }
+
+    bool File::Read(char* buffer, size_t count)
+    {
+        if(m_Open && m_ReadMode)
+        {
+            fread(buffer, 1, count, m_FileHandle);
+            return true;
+        }
+        return false;
+    }
+
+    bool File::Read(unsigned char* buffer, size_t count)
+    {
+        if(m_Open && m_ReadMode)
+        {
+            fread(buffer, 1, count, m_FileHandle);
+            return true;
+        }
+        return false;
+    }
+
+    const size_t File::FileSize() const
+    {
+        size_t cur = ftell(m_FileHandle);
+
+        fseek(m_FileHandle, 0, SEEK_END);
+        size_t size = ftell(m_FileHandle);
+        fseek(m_FileHandle, 0, cur);
+
+        return size;
     }
 
     // Private methods
@@ -121,6 +159,7 @@ namespace fileio
             case io::read | io::noerase:
             case io::read:
                 fopen_imp(path, "r", m_FileHandle);
+                m_ReadMode = true;
                 break;
             
             case io::write | io::app:
@@ -136,6 +175,7 @@ namespace fileio
             case io::binary:
             case io::read | io::binary:
                 fopen_imp(path, "rb", m_FileHandle);
+                m_ReadMode = true;
                 break;
             
             case io::write | io::binary:
@@ -150,23 +190,27 @@ namespace fileio
             
             case io::write | io::read:
                 fopen_imp(path, "w+", m_FileHandle);
+                m_ReadMode = true;
                 m_WriteMode = true;
                 break;
             
             case io::write | io::read | io::noerase:
             case io::app | io::read:
                 fopen_imp(path, "a+", m_FileHandle);
+                m_ReadMode = true;
                 m_WriteMode = true;
                 break;
             
             case io::write | io::read | io::binary:
                 fopen_imp(path, "w+b", m_FileHandle);
+                m_ReadMode = true;
                 m_WriteMode = true;
                 break;
             
             case io::write | io::read | io::noerase | io::binary:
             case io::app | io::read | io::binary:
                 fopen_imp(path, "a+b", m_FileHandle);
+                m_ReadMode = true;
                 m_WriteMode = true;
                 break;
             
